20e0f908dcec7efebec67b4e731bc52a3e760b6e
Sachin Kumar <sachinkum123567@gmail.com>
2025-10-31 19:51:49 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from cat-vs-dog!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ models/model.py
@@ -0,0 +1,36 @@
+"""
+CNN model to classify cats and dogs
+"""
+
+from torch import nn
+from torch.nn import functional as F
+
+class CnnModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=(5,5), stride=2, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(5,5), stride=2, padding=1)
+        self.conv3 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(5,5), padding=1)
+
+        self.fc1 = nn.Linear(in_features=64*6*6, out_features=500)
+        self.fc2 = nn.Linear(in_features=500, out_features=50)
+        self.fc3 = nn.Linear(in_features=50, out_features=2)
+
+    
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2)
+
+        x = F.relu(self.conv2(x))
+        x = F.max_pool2d(x, 2)
+
+        x = F.relu(self.conv3(x))
+        x = F.max_pool2d(x, 2)
+
+        x = x.view(x.shape[0], -1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+
+        return x
+
